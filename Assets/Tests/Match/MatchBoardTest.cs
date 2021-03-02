9626dff9936451cb387b77e38dce5919b563d338
Hyago Oliveira <hyagogow@gmail.com>
2021-03-02 12:58:26 -0300

    Add basic MatchBoardTest class

---
+++ Assets/Tests/Match/MatchBoardTest.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace Bejeweled.Macth.Tests
+{
+    public class MatchBoardTest
+    {
+        private MatchBoard board;
+        private MatchLevelSettings levelSettings;
+
+        [SetUp]
+        public void Setup()
+        {
+            CreateLevelSettings();
+            InstanciateBoardPrefab();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(levelSettings);
+            Object.Destroy(board.gameObject);
+        }
+
+        [Test]
+        public void Board_WhenCreated_HasSameSizeFromLevelSettings()
+        {
+            var actualSize = board.GetSize();
+            var expectedSize = levelSettings.BoardSize;
+
+            Assert.AreEqual(expectedSize, actualSize);
+        }
+
+        [Test]
+        public void Board_WhenCreated_HasSamePieceCountFromLevelSettings()
+        {
+            var actualPiecesCount = board.PieceManager.PiecesCount;
+            var expectedPiecesCount = levelSettings.PiecesCount;
+
+            Assert.AreEqual(expectedPiecesCount, actualPiecesCount);
+        }
+
+        private void CreateLevelSettings()
+        {
+            levelSettings = ScriptableObject.CreateInstance<MatchLevelSettings>();
+        }
+
+        private void InstanciateBoardPrefab()
+        {
+            const string path = "Assets/Prefabs/Match/MatchBoard.prefab";
+            var boardPrefab = PrefabUtility.LoadPrefabContents(path);
+            board = Object.Instantiate(boardPrefab).GetComponent<MatchBoard>();
+        }
+    }
+}
