995b8d9883ad9c47e76d2691902f74290ccb3d89
Hyago Oliveira <hyago.oliveira@tapps.games>
2021-03-02 17:23:09 -0300

    Add Initial Match test

--- Assets/Tests/Match/MatchBoardTest.cs
+++ Assets/Tests/Match/MatchBoardTest.cs
@@ -33,6 +33,32 @@ namespace Bejeweled.Tests.Macth
         }
 
         [Test]
+        public void Board_WhenPopulated_HasNoInitialMatch()
+        {
+            var size = board.GetSize();
+
+            for (int y = 0; y < size.y; y++)
+            {
+                for (int x = 0; x < size.x; x++)
+                {
+                    var currentPieceId = board.GetPieceIdAt(x, y);
+
+                    var rightmostPieceId = board.GetPieceIdAt(x + 1, y);
+                    var rightfarPieceId = board.GetPieceIdAt(x + 2, y);
+
+                    var topmostPieceId = board.GetPieceIdAt(x, y + 1);
+                    var topfarPieceId = board.GetPieceIdAt(x, y + 2);
+
+                    var isVertMatch = currentPieceId == topmostPieceId && currentPieceId == topfarPieceId;
+                    var isHortMatch = currentPieceId == rightmostPieceId && currentPieceId == rightfarPieceId;
+
+                    Assert.IsFalse(isVertMatch, "A vertical match was detect when populating a board.");
+                    Assert.IsFalse(isHortMatch, "A horizontal match was detect when populating a board.");
+                }
+            }
+        }
+
+        [Test]
         public void Board_WhenCreated_HasSamePieceCountFromLevelSettings()
         {
             var actualPiecesCount = board.PieceManager.PiecesCount;
