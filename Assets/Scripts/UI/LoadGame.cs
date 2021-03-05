214eb5a6f56a9ff414f230c7e398313a351e7a5b
Hyago Oliveira <hyago.oliveira@tapps.games>
2021-03-05 14:19:18 -0300

    Rename MainMenu component to LoadGame

---
+++ Assets/Scripts/UI/LoadGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using ActionCode.SceneManagement;
+
+namespace Bejeweled.UI
+{
+    /// <summary>
+    /// Loads the games Scene.
+    /// </summary>
+    public sealed class LoadGame : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The game Scene."), Scene]
+        private string gameScene = "Game";
+        [SerializeField, Tooltip("The Scene Loading Settings asset.")]
+        private SceneLoadingSettings settings;
+
+        /// <summary>
+        /// Loads the game scene fading the screen.
+        /// </summary>
+        public void GoToGameScene() => SceneManager.LoadScene(gameScene, settings);
+    }
+}
