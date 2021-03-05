03793e53018df6e040628ec615c7380a6d777d82
Hyago Oliveira <hyagogow@gmail.com>
2021-03-04 23:08:24 -0300

    Replace MatchLevelManager by LevelManager Component

--- Assets/Scripts/UI/LevelManager.cs
+++ Assets/Scripts/UI/LevelManager.cs
@@ -1,30 +1,38 @@
 ﻿using UnityEngine;
-using Bejeweled.UI;
+using Bejeweled.Macth;
 
-namespace Bejeweled.Macth
+namespace Bejeweled.UI
 {
+    /// <summary>
+    /// Level Manager component.
+    /// <para>You <see cref="NextLevel"/> and <see cref="PreviousLevel"/> function to switch between level.</para>
+    /// </summary>
     [DisallowMultipleComponent]
-    public sealed class MatchLevelManager : MonoBehaviour
+    public sealed class LevelManager : MonoBehaviour
     {
+        [SerializeField, Tooltip("The local SliderBar component.")]
+        private SliderBar scoreBar;
         [SerializeField, Tooltip("The level settings assets.")]
         private MatchLevelSettings[] levelSettings = new MatchLevelSettings[0];
 
+        /// <summary>
+        /// The current MatchLevelSettings used to populate the board. 
+        /// </summary>
         public MatchLevelSettings CurrentLevelSettings
             => levelSettings[currentLevelSettingsIndex];
 
+        /// <summary>
+        /// Total level count.
+        /// </summary>
         public int LevelsCount => levelSettings.Length;
 
         private MatchBoard board;
-        private SliderBar scoreBar;
-
         private int currentLevelSettingsIndex = 0;
 
         private void Awake()
         {
             board = FindObjectOfType<MatchBoard>();
-            scoreBar = FindObjectOfType<SliderBar>();
-
-            UpdateScoreBar();
+            InitializeScoreBar();
         }
 
         private void OnEnable()
@@ -37,33 +45,35 @@ namespace Bejeweled.Macth
             if (board) board.OnIncreaseScore -= IncreaseScore;
         }
 
+        /// <summary>
+        /// Go to the next level.
+        /// </summary>
         public void NextLevel()
         {
             currentLevelSettingsIndex++;
             if (currentLevelSettingsIndex >= LevelsCount)
                 currentLevelSettingsIndex = 0;
 
-            UpdateScoreBar();
+            if (board) board.Populate(CurrentLevelSettings);
+            InitializeScoreBar();
         }
 
+        /// <summary>
+        /// Go to the previous level.
+        /// </summary>
         public void PreviousLevel()
         {
             currentLevelSettingsIndex--;
             if (currentLevelSettingsIndex < 0)
                 currentLevelSettingsIndex = LevelsCount - 1;
 
-            UpdateScoreBar();
+            if (board) board.Populate(CurrentLevelSettings);
+            InitializeScoreBar();
         }
 
-        private void IncreaseScore(int score)
-        {
-            if (scoreBar) scoreBar.CurrentValue += score;
-        }
+        private void IncreaseScore(int score) => scoreBar.CurrentValue += score;
 
-        private void UpdateScoreBar()
-        {
-            if (!scoreBar) return;
-            scoreBar.Initialize(0F, CurrentLevelSettings.totalScore);
-        }
+        private void InitializeScoreBar()
+            => scoreBar.Initialize(0F, CurrentLevelSettings.totalScore);
     }
 }
