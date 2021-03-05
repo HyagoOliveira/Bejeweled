3bf34cf7708b04263c99bc2d16e472462575d97c
Hyago Oliveira <hyagogow@gmail.com>
2021-03-04 21:39:54 -0300

    Add MainMenu component

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Bejeweled.UI
+{
+    public sealed class MainMenu : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The game Scene name.")]
+        private string gameSceneName = "Game";
+
+        public void GoToGameScene() => SceneManager.LoadScene(gameSceneName);
+    }
+}
